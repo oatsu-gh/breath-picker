93a0b62f27577ef0b8736ab5d28d486edf2f76f6
unknown <panchi.psp@gmail.com>
2020-06-18 04:20:48 +0900

    検証とかちゃんとやった。つくった。

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# BreathPicker
+# breath picker
+
  歌唱DBからブレス音声を切り出す
+
+
+
+## 開発環境
+
+Window10 Education 2004, Python3.8.3
+
+
+
+## 使用ライブラリ
+
+- Pydub（標準ライブラリだと思う。たぶん。）
+
+- [utaupy](https://github.com/oatsu-gh/utaupy)（UTAUや歌唱データベース用の自作ライブラリです。）
+
+## 使い方
+
+- PowerShellとかから `python breath_picker.py` で起動してがんばってください。切り出したファイルは (currentdirectory)/out 以下に保存されます。 
+- 「東北きりたん歌唱データベース」形式のモノフォンラベルは、[こちら](https://github.com/oatsu-gh/oto2lab/blob/master/tool/convert_label_time_unit.py)のツールで単位換算してから本ツールで使ってください。
+- 歌唱データベースの音声そのものをDTMなどの素材として使うことは、規約で制限されていることが多いです。
+- 本ツールで生成したファイルをDTMなどに使いたい場合は、各歌唱データベースの規約に準拠しているか必ず確認してください。きりたんはNGだと思います。
---
+++ breath_picker.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+歌唱データベースの音声ファイルからブレスを切り出すスクリプト
+"""
+
+import os
+import sys
+from glob import glob
+
+from pydub import AudioSegment
+
+from utaupy import label as _label
+
+TARGET_PHONEME = 'br'
+
+
+def main():
+    """
+    音声ファイルとラベルファイルを指定
+    ラベルファイルを読み取る
+    ラベルファイルのうち特定の音素の開始・終了時刻を取得
+    音声ファイルを切断
+    音声ファイルを保存
+    """
+    labdir = input('LABのフォルダを指定してください : ').strip(r'"')
+    wavdir = input('WAVのフォルダを指定してください : ').strip(r'"')
+    dirname = input('目印にしたい文字列を入力してください :')
+    labfiles = glob(f'{labdir}/*.lab')
+    wavfiles = glob(f'{wavdir}/*.wav')
+    # 曲ごとのフォルダな場合は再帰的に検索
+    if max(len(labfiles), len(wavfiles)) == 0:
+        print('  LABとWAVが0個なので再帰的に検索して続行します。')
+        labfiles = glob(f'{labdir}/**/*.lab', recursive=True)
+        wavfiles = glob(f'{wavdir}/**/*.wav', recursive=True)
+    # ファイルの個数が合わないときはエラーで終了
+    if len(labfiles) != len(wavfiles):
+        print(f'  LABファイルとWAVファイルの個数が合いません。({len(labfiles)}, {len(wavfiles)})')
+        print('  ファイル名が対応していること、個数が一致することを確認してやり直してください。')
+        input('Enterを押すと終了します。')
+        sys.exit()
+
+    print(f'対象のLABとWAVは{len(labfiles)}個です。')
+    print('処理を始めます。')
+    os.makedirs(f'./out/{dirname}', exist_ok=False)
+    for labfile, wavfile in zip(labfiles, wavfiles):
+        small_label = extract_label(labfile, TARGET_PHONEME)
+        cutout_wav(wavfile, small_label, dirname)
+    print('処理が終わりました。')
+    input('Enterを押すと終了します。')
+
+
+def extract_label(labfile, target_phoneme):
+    """切り出したい音素のみのラベルを返す(単位は100ns)"""
+    label_values = _label.load(labfile).values
+
+    l = []  # [[音素, 開始時刻, 終了時刻], [], ...]
+    for v in label_values:
+        if v[2] == target_phoneme:
+            l.append(v)
+    return l
+
+
+def cutout_wav(wavfile, l, dirname):
+    """
+    wavfile中の指定区間を切り出す。
+    l は [[音素, 開始時刻, 終了時刻], [], ...] の2次元リスト
+    """
+    songname = os.path.splitext(os.path.basename(wavfile))[0]
+    # wavファイルの読み込み
+    sound = AudioSegment.from_file(wavfile, format='wav')
+
+    # ミリ秒で区間指定して抽出
+    for i, v in enumerate(l):
+        # 100ns(10^-7)をms(10^-3)に変換
+        t_start = int(v[0] // 10000)
+        t_end = int(v[1] // 10000)
+        # 出力パスを設定
+        # './out/(任意文字列)/(音素)_(元の名前)_000001.wav'
+        outpath = f'./out/{dirname}/{v[2]}_{songname}_{(i+1):06}.wav'
+        # '(出力パス)tab(音声の長さ)'
+        print(f'    {outpath}\t{t_end - t_start}ms')
+        # 指定音素区間を切り出し
+        cutsound = sound[t_start:t_end]
+        # ファイル出力
+        cutsound.export(outpath, format='wav')
+
+
+if __name__ == '__main__':
+    main()
